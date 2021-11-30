cfedae94741ee892a495f862541cf6c62f933cad
Iman Irajdoost <iman.milo@yahoo.com>
2021-11-30 13:17:34 +0100

    added easter egg

---
+++ Assets/EasterEggManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggManager : MonoBehaviour
+{
+    private bool hasClicked = false;
+    public GameObject target;
+    public float speed = 1f;
+
+    public Camera cam;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && !hasClicked)
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                hasClicked = true;
+            }
+        }
+
+        if (hasClicked)
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
