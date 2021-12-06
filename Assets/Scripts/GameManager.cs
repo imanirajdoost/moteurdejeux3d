c1eb6fceed2cab9d1a401a3d4bd6de77f6ccbdb7
Ahmad JREDA <ahmad.jreda@etu.unistra.fr>
2021-12-06 08:34:51 +0100

    fin du projet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -146,6 +146,7 @@ public class GameManager : MonoBehaviour
                     startCutsceneManager.StartCutscene();
                 }
             }
+     
                 return;
         }
         if (CanRestart)
